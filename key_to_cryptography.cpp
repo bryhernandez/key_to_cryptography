2a7fa1d560aefa2b0c27238d0340c1a7469be963
bryhernandez <61296527+bryhernandez@users.noreply.github.com>
2020-11-13 11:05:13 -0800

    Update key_to_cryptography.cpp

--- key_to_cryptography.cpp
+++ key_to_cryptography.cpp
@@ -2,23 +2,28 @@
 #include <string>
 using namespace std;
 
-int shift(char x, char y){
-  return (26 + (x - 'A') - (y - 'A')) % 26 + 'A';
-}
-
 int main() {
   string word;
   string code;
-  
+
   cin >> word;
   cin >> code;
 
   string cypher = "";
 
   for(int i = 0; i < word.size(); i ++){
-    char letter = shift(word[i], code[i]);
-    cypher.push_back(letter);
-    code += string(1, letter);
+    int index = i % code.size();
+
+    int num = (word[i] - 'A') - (code[index] - 'A');
+
+    if(num >= 26)
+      num -= 26;
+    if(num <= 0)
+      num += 26;
+
+    word[i] = num + 'A';
+    code[index] = word[i];
   }
-  cout << cypher;
+
+  cout << word;
 }
